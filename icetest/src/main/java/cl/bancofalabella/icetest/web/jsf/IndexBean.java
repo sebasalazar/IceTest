62f89a46cce5b50e05bfff7f1b75a2a2eee9515b
Sebastián Salazar Molina <sebasalazar@gmail.com>
2017-04-27 15:47:43 -0300

    Modificación para pruebas

--- icetest/src/main/java/cl/bancofalabella/icetest/web/jsf/IndexBean.java
+++ icetest/src/main/java/cl/bancofalabella/icetest/web/jsf/IndexBean.java
@@ -1,12 +1,10 @@
 package cl.bancofalabella.icetest.web.jsf;
 
-import cl.bancofalabella.icetest.utils.FacesUtils;
 import java.io.Serializable;
+import java.util.Date;
 import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,29 +12,20 @@ import org.springframework.stereotype.Component;
  * @author Sebastián Salazar Molina.
  */
 @Component
-@Scope("request")
-@Qualifier("indexBean")
 public class IndexBean implements Serializable {
 
     private static final long serialVersionUID = 8491384550722840576L;
 
-    private String token = null;
-    private String loginUrl = null;
+    private Date now = null;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(IndexBean.class);
 
     @PostConstruct
     public void initBean() {
-        loginUrl = String.format("%s/login", FacesUtils.getRequest().getContextPath());
-        token = FacesUtils.getRequestParameter("token");
-        LOGGER.debug("Se ha recibido el token: '{}'", token);
+        now = new Date();
     }
 
-    public String getToken() {
-        return token;
-    }
-
-    public String getLoginUrl() {
-        return loginUrl;
+    public Date getNow() {
+        return now;
     }
 }
