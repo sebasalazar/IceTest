996e030eefb7bf04279ee5c696139834d6a3dc44
Sebastián Salazar Molina <sebasalazar@gmail.com>
2017-04-27 12:02:36 -0300

    Avance inicial

---
+++ icetest/src/main/java/cl/bancofalabella/icetest/model/BaseBean.java
@@ -0,0 +1,22 @@
+package cl.bancofalabella.icetest.model;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ *
+ * @author Sebastián Salazar Molina
+ */
+@XmlRootElement
+public class BaseBean implements Serializable {
+
+    private static final long serialVersionUID = -572159829033690039L;
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.reflectionToString(
+                this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+}
